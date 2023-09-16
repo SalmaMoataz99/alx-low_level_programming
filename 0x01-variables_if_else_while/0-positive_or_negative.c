c4cdaf2b8e6be42ca6f82d3f587ebb3568b2ffd2
salma <salma.moataz3499@gmail.com>
2023-09-16 09:51:11 +0300

    a

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,53 @@
+#!/bin/bash
+#include <stdio.h>
+void main (void)
+{
+
+	int n;
+	printf("Enter your number:\n");
+	scanf("%d",&n);
+	if (n==0)
+
+		printf("ZERO\n");
+		
+	else if(n<0)
+	printf("Negative\n");
+	else 
+	printf("Positive\n");	
+	
+
+
+
+
+
+
+}	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
